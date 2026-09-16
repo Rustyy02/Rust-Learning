53ae82b83773d6e236c40d4e13bf6b7f87ee4179
Rustyy02 <alvaroafa1@gmail.com>
2026-09-16 16:43:17 -0300

    mass

--- rustlings/exercises/07_structs/structs1.rs
+++ rustlings/exercises/07_structs/structs1.rs
@@ -1,9 +1,15 @@
 struct ColorRegularStruct {
-    // TODO: Add the fields that the test `regular_structs` expects.
-    // What types should the fields have? What are the minimum and maximum values for RGB colors?
+    red: i32,
+    green: i32,
+    blue: i32, // TODO: Add the fields that the test `regular_structs` expects.
+               // What types should the fields have? What are the minimum and maximum values for RGB colors?
 }
 
-struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+struct ColorTupleStruct(i32, i32, i32);
+
+// Estas dos anteriores son dos formas distintas de estructurar una "clase".
+//
+//
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -20,6 +26,12 @@ mod tests {
     fn regular_structs() {
         // TODO: Instantiate a regular struct.
         // let green =
+        // con la forma 1
+        let green = ColorRegularStruct {
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -30,6 +42,8 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct.
         // let green =
+        // con la forma 2
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -40,6 +54,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
         // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
